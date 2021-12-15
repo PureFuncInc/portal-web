1a7e691cc7ca74f02300bcd62fb47a6514f838f5
Nelson Liu <nelsliu9121@gmail.com>
2021-12-15 17:01:54 +0800

    refactor: remove obsolete file

--- src/utilities/generateRandomColor.ts
+++
@@ -1,9 +0,0 @@
-export function generateRandomColor(): string {
-  const colors = Array(3)
-    .fill(255)
-    .map(v => Math.round(Math.random() * v)
-      .toFixed(0))
-    .join(', ')
-
-  return `rgb(${colors})`
-}
